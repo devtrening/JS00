f29c27e6024a592cb1fb8064d274ceeb29e733dd
Darko Vidak <darko@corpus-online.com>
2012-10-24 19:14:05 +0200

    još jedna versija

--- lesson_3.js
+++ lesson_3.js
@@ -7,6 +7,14 @@ var random= function(){
 		this[rnd] = tmp;
 	}
 };
+
+var rand1 = function (){
+	this.sort(function(){
+		return Math.sin(Math.PI*Math.random()*2);
+	})
+	return this;
+}
+
 a = new Array();
 a.push(4,5,6,3,2,1,7,6,55,6,65,12,21,34);
 console.log("novi: ",a);
@@ -14,7 +22,7 @@ document.write(a+"<br>");
 a.sort(function(c,b){return c-b});
 document.write(a+"<br>");
 console.log("sortirani: ", a);
-a.random = random;
+a.random = rand1;
 a.random();
 document.write(a+"<br>");
 console.log("random: ", a);

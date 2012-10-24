4a8da6dff730db9ed1b79e845ee632c8dde04418
Marijan Grofelnik <dr.marijan@gmail.com>
2012-10-24 19:17:24 +0200

    lesson_3, zadatak sa random sortiranjem

--- lesson_3.js
+++ lesson_3.js
@@ -1,1 +1,15 @@
 // lesson_3.js
+
+a = [5,7,17,45,11,0,6];
+
+a.random =function() {
+	
+	var i = 0,
+	    max = this.length;
+		
+		for (i; i<max; i+=1) {
+			console.log(this[i])};
+	
+	return this;
+
+}

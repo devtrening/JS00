37160a8d2937e5332687e155420efb68da1ac709
NatasaSaric <natasa.saric33@gmail.com>
2012-10-24 19:16:48 +0200

    bok bok

--- lesson_3.js
+++ lesson_3.js
@@ -1,1 +1,25 @@
 // lesson_3.js
+
+var a=[5, 1, 10, 15, 20, 25];
+
+a.random = function () {
+var i = 0,
+max = this.length;
+
+for (i; i < max; i += 1) {
+	console.log (this[i] );
+}
+//return this;
+};
+
+a.random = function () {
+"use strict";
+this.sort (function) {return Math.sin};
+
+for (i; i < max; i += 1) {
+	console.log (this[i] );
+}
+//return this;
+};
+
+

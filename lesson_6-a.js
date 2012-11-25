f684fdf06fbd888b988625cd8d3d6017ef1b81d1
Luka Muzinic <luka@muzinic.net>
2012-11-26 00:15:34 +0100

    Lesson 6: Storage and JSON --- Finished code samples.

--- lesson_6-a.js
+++ lesson_6-a.js
@@ -1,5 +1,52 @@
-// lesson_6.js
+// lesson_6-a.js
 (function () {
     "use strict";
 
+    if (window.sessionStorage) {
+        console.log('sessionStorage available!');
+    } else {
+        console.log('Fallback to using cookies!');
+    }
+       
+    var user = sessionStorage.getItem('user');
+    user = JSON.parse(user);
+    
+    // sto ako nam ne trebaju svi podaci?
+    var userPart = JSON.stringify(user, ['username', 'email']);
+    
+    // sto ako je potrebno manipulirati podacima?
+    userPart = JSON.stringify(user, function(key, value) {
+        if (key === 'luckyNumbers') {
+            return [13];
+        } else {
+            return value; // uvijek treba pokriti defaultnu vrijednost
+        } 
+    });
+    
+    // modificiranje izgleda
+    userPart = JSON.stringify(user, null, '-');
+    
+    // kada vlasnik objekta zeli definirati kako se pretvara u string
+    user.toJSON = function() {
+        this.email = this.email.replace(/\w/g, '*');
+        return this;
+    };
+    
+    userPart = JSON.stringify(user);
+    
+    // parsing
+    user = JSON.parse(userPart, function(key, value) {
+        if (key === 'luckyNumbers') {
+            var luckyNumbers = [];
+            for (var i = 0; i < 7; i += 1) {
+                luckyNumbers[i] = Math.round(Math.random() * 100);
+            }
+            return luckyNumbers;
+        } else {
+            return value;
+        }
+    });
+    
+    console.log(user);
+    
 }());
--- lesson_6.js
+++ lesson_6.js
@@ -2,4 +2,44 @@
 (function () {
     "use strict";
 
+    if (window.sessionStorage) {
+        console.log('sessionStorage available!');
+    } else {
+        console.log('Fallback to using cookies!');
+    }
+    
+    document.addEventListener('storage', function(event) {
+        alert('storage');
+    });
+
+    // store a value
+    sessionStorage.setItem('user', 'lmuzinic');
+    sessionStorage.setItem('email', 'luka@muzinic.net');
+
+    // get a value
+    var user = sessionStorage.getItem('user');
+
+    // get a value by position
+    var email_key = sessionStorage.key(0);
+    
+    console.log(user, email_key);
+    
+    // remove all
+    sessionStorage.clear();
+    
+    console.log(
+        sessionStorage.getItem('email')
+    ); // should be null
+        
+    user = {
+        username : 'lmuzinic',
+        email : 'luka@muzinic.net',
+        'luckyNumbers' : [5, 8, 77, 122]
+    };
+    
+    // what now Batman? Zasto je vrijednost identicna user.toString();?
+    sessionStorage.setItem('user', user);
+    
+    sessionStorage.setItem('user', JSON.stringify(user));
+    
 }());

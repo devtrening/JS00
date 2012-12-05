ab9b18a846eb21c921649b02da3ef9390e7ccfc0
NatasaSaric <natasa.saric33@gmail.com>
2012-12-05 19:44:25 +0100

    nesto malo

--- examples/shopping_list.js
+++ examples/shopping_list.js
@@ -1,5 +1,27 @@
 // shopping_list.js
 (function () {
-    "use strict";
-
+    g;
+	var inpNewItem,
+	btnAddNewItem,
+	arrShopingList[];
+	
+	var position = arrShoppingList.length;
+	arrShoppingList[position] =newItem;
+	
+	inpNewItem = document.getElementById("inpNewItem");
+	btnAddNewItem = document.getElementById("btnAddNewItem");
+	
+	btnAddNewItem.addEventListener("click", function(){
+		if (inpNewItem.value.trim() !== "") {
+		arrShoppingList.push(inpNewItem.value.trim());
+		
+		var divNewItem = document.createElement("div");
+		divNewItem.setAtribute ("class","alert");
+		divNewItem.innerHTML = inpNewItem.value.trim();
+		
+		btnClose.setAtribute ("class","close");
+	
+		
+		console:log("Zabilježeno")}
+		), false);
 }());

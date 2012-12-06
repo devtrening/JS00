8f9009097526766ff625363e091ebd0d9ef96c73
Marijan Grofelnik <dr.marijan@gmail.com>
2012-12-06 01:24:40 +0100

    lista - popravljeno malo

--- examples/shopping_list.js
+++ examples/shopping_list.js
@@ -1,67 +1,76 @@
 // shopping_list.js
 (function () {
     "use strict";
-	
-	var inpNewItem,
-		btnAddNewItem,
-		divItemContainer,
-		arrShoppingList = [];
-	
-	inpNewItem = document.getElementById('inpNewItem');
-	btnAddNewItem = document.getElementById('btnAddItem');
-	divItemContainer = document.getElementById('divItemContainer')
-	
-	btnAddNewItem.addEventListener('click', function(){
-	console.log('blabla');
-		if(inpNewItem.value.trim !== ''){
-			var newItem  = {
-				caption : inpNewItem.value.trim(),
-				checked :0
-			};
-			
-			var position = arrShoppingList.lenght;
-			arrShoppingList[position] = newItem;
-		
-		var divNewItem = document.createElement('div');
-		divNewItem.setAttribute('class', 'alert');
-		divNewItem.setAttribute('data-position', 'position');
-		divNewItem.innerHTML = newItem.caption;
-		
-		var btnClose = document.createElement('button');
-		btnClose.setAttribute('type', 'button');
-		btnClose.setAttribute('class', 'close');
-		btnClose.innerHTML = 'X';
-		btnClose.addEventListener('click', function () {
-			this.parentNode.setAttribute('class', 'alert alert-error');
-			
-			var position = this.parentNode.getAttribute('data-position');
-			if ( arrShoppingList[position].checked === 1){
-				 arrShoppingList[position].checked = 0;
-			} else {
-				arrShoppingList[position].checked = 1;
-				this.parentNode.setAttribute('class', 'alert alert-error');
-			}
-			console.log(this.parentNode);
-		},false);
-		
-		divNewItem.appendChild(btnClose);
-			
-		divItemContainer.appendChild(divNewItem);
-		
-		inpNewItem.value = '';
-		} else {
-		console.log('Please enter avalue!');
-		}
-	},false);
-	
-	btnCleanList.addEventListener('click', function(){
-		var i = arrShoppingList.length;
-		while (i>1){
-			if ( arrShoppingList[i].checked === 1) {
-				arrShoppingList[i].divNewItem.parentNode.removeChild(arrShopingList[i].divNewItem);
-				i--;
-			}
-		}
-	},false);
-	
+
+    var inpNewItem,
+        btnAddNewItem,
+        btnCleanList,
+        divItemContainer,
+        arrShoppingList = [];
+    
+    inpNewItem = document.getElementById('inpNewItem');
+    btnAddNewItem = document.getElementById('btnAddNewItem');
+    btnCleanList = document.getElementById('btnCleanList');
+    divItemContainer = document.getElementById('divItemContainer');
+    
+    btnAddNewItem.addEventListener('click', function(){
+        if (inpNewItem.value.trim() !== '') {
+            var newItem = {
+                caption : inpNewItem.value.trim(),
+                checked : 0,
+                divNewItem : {}
+            };
+            
+            var position = arrShoppingList.length;
+            arrShoppingList[position] = newItem;
+
+            var divNewItem = document.createElement('div');
+            divNewItem.setAttribute('class', 'alert');
+            divNewItem.setAttribute('data-position', position);
+            divNewItem.innerHTML = newItem.caption;
+            
+            arrShoppingList[position].divNewItem = divNewItem;
+            
+            var btnClose = document.createElement('button');
+            btnClose.setAttribute('type', 'button');
+            btnClose.setAttribute('class', 'close');
+            btnClose.innerHTML = '<i class="icon-exclamation-sign"></i>';
+            btnClose.addEventListener('click', function(){
+                var position = this.parentNode.getAttribute('data-position');
+                console.log(position);
+                if (arrShoppingList[position].checked === 1){
+                    arrShoppingList[position].checked = 0;
+                    this.parentNode.setAttribute('class', 'alert');
+					btnClose.innerHTML = '<i class="icon-exclamation-sign"></i>';
+                } else {
+                    arrShoppingList[position].checked = 1;
+                    this.parentNode.setAttribute('class', 'alert alert-success');
+					this.innerHTML = '<i class="icon-ok-sign"></i>';
+					}
+      
+                console.log(arrShoppingList);
+            }, false);
+            
+            divNewItem.appendChild(btnClose);
+            
+            divItemContainer.appendChild(divNewItem);
+            
+            
+            inpNewItem.value = '';
+        } else {
+            console.log('Please enter a value');
+        }
+    }, false);
+    
+    btnCleanList.addEventListener('click', function(){
+        var i = arrShoppingList.length - 1;
+        while (i >= 0){
+            if (arrShoppingList[i].checked === 1){
+                arrShoppingList[i].divNewItem.parentNode.removeChild(arrShoppingList[i].divNewItem);
+                arrShoppingList.splice(i, 1);
+                console.log(arrShoppingList);
+            }
+            i -= 1;
+        }
+    }, false);
 }());

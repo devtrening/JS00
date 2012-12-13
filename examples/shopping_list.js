0cba762041ec3d40500d0184366e2e4b447e3a89
Darko Vidak <darko@corpus-online.com>
2012-12-13 13:48:11 +0100

    dodan edit distance u typeahead tak da nudi i stringove koji se razlikuju (utipkano krivo slovo, fali slovo, viška slovo)

--- examples/shopping_list.js
+++ examples/shopping_list.js
@@ -25,8 +25,7 @@
 			if (!this.isInList(word.toUpperCase())){
 				this.list.push(word);				
 				this.save();
-				this.update();
-				console.log(this.list);
+				//this.update();				
 			}
 		},
 		
@@ -47,14 +46,44 @@
 			var tmp = JSON.parse(localStorage.getItem('WordsList'));
 			if (tmp !== null){
 				this.list = tmp;
-				this.update();
+				//this.update();
 			};			
 		},
-		update : function () {
+		//update više netreba, typeahead je aktiviran dolje i koristi source funkciju
+		/*update : function () {
 			inpNewItem.setAttribute('data-provide', 'typeahead');
 			inpNewItem.setAttribute('data-items', '5');
 			inpNewItem.setAttribute('data-source', JSON.stringify(this.list));
+		},*/
+		getEditDistance : function (a, b){
+			if(a.length == 0) return b.length; 
+			if(b.length == 0) return a.length; 
+			var matrix = [];
+			// increment along the first column of each row
+			var i;
+			for(i = 0; i <= b.length; i++){
+				matrix[i] = [i];
+			} 
+			// increment each column in the first row
+			var j;
+			for(j = 0; j <= a.length; j++){
+				matrix[0][j] = j;
+			}
+			// Fill in the rest of the matrix
+			for(i = 1; i <= b.length; i++){
+				for(j = 1; j <= a.length; j++){
+					if(b.charAt(i-1) == a.charAt(j-1)){
+						matrix[i][j] = matrix[i-1][j-1];
+					} else {
+						matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+						Math.min(matrix[i][j-1] + 1, // insertion
+						matrix[i-1][j] + 1)); // deletion
+					}
+				}
+			} 
+			return matrix[b.length][a.length];
 		}
+		
 	}
 	
     var Item = function(caption) {
@@ -227,4 +256,22 @@
         objItemList.addItem(localList[i].caption);
     }
     WordsList.load();
+	
+	// neznam kak drugačije uglavit typeahead , rado bi ga strpao u WordsList al bi onda vjerojatno imao problema sa "this"
+	$("#inpNewItem").typeahead({
+        minLength: 3,
+        source: function(query, process) {			            
+			return WordsList.list;
+        },
+		matcher : function(item){
+			// originalna funkcija
+			var result= ~item.toLowerCase().indexOf(this.query.toLowerCase());			
+			if (!result){
+				// edit distance
+				result = WordsList.getEditDistance(item,this.query)<4;
+			};
+			return  result;
+			
+		}
+    });
 }());
